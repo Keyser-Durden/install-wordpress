27a44f9b394b61230efdfd6ab25a339e556de271
SquanchIt <141355723+SquanchIt@users.noreply.github.com>
2023-09-09 22:06:26 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,11 @@ download_wordpress() {
  sudo chown www-data: /opt/www
  curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /opt/www
 }
+
+enable_site() {
+ a2ensite wordpress
+ a2enmod rewrite
+ a2dissite 000-default
+ service apache2 reload
+}
+ 
