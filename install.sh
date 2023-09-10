7c3031a617f8970ffb90657a18d162252cf28e4a
SquanchIt <141355723+SquanchIt@users.noreply.github.com>
2023-09-09 22:24:25 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,6 +15,9 @@ enable_site() {
  
 configure_db() {
  echo "Nothing here yet"
+ # CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ # GRANT ALL ON wordpress.* TO 'wordpress_user'@'localhost' IDENTIFIED BY 'password';
+ # FLUSH PRIVILEGES;
 }
 
 setup_firewall() {
